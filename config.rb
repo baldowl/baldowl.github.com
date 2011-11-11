0e896f33955d0cf2236b4de49b68b239e518f539
Emanuele Vicentini <emanuele.vicentini@gmail.com>
2011-11-11 19:24:48 +0100

    Started to play with Sass & Compass.

---
+++ config.rb
@@ -0,0 +1,8 @@
+http_path = '/'
+css_dir = 'stylesheets'
+sass_dir = 'stylesheets/sass'
+images_dir = 'images'
+javascripts_dir = 'javascripts'
+output_style = :expanded
+line_comments = false
+preferred_syntax = :sass
